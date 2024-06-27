26cc707b67b470f86d1886583b0f93c2da678dbb
Sridhar Jonnalagadda <jonnalagadds@juniper.net>
2024-06-27 15:30:07 -0700

    Replacing angled brackets with quotes to fix compiler issues

--- src/oatpp/web/server/handler/AuthorizationHandler.hpp
+++ src/oatpp/web/server/handler/AuthorizationHandler.hpp
@@ -26,7 +26,7 @@
 #ifndef oatpp_web_server_handler_AuthorizationHandler_hpp
 #define oatpp_web_server_handler_AuthorizationHandler_hpp
 
-#include <oatpp/web/protocol/http/incoming/Request.hpp>
+#include "oatpp/web/protocol/http/incoming/Request.hpp"
 #include "oatpp/web/protocol/http/Http.hpp"
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/data/mapping/type/Type.hpp"
